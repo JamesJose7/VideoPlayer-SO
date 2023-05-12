6b875ec3d5aa7b963bc9a2e26eeb4950683742e9
José Eduardo Eguiguren <pepe.96.jose@gmail.com>
2023-05-12 12:51:27 -0700

    Add exo video player and load api videos once fetched

--- app/src/main/java/com/silverorange/videoplayer/ui/videos/MainActivity.kt
+++ app/src/main/java/com/silverorange/videoplayer/ui/videos/MainActivity.kt
@@ -3,12 +3,17 @@ package com.silverorange.videoplayer.ui.videos
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
 import com.silverorange.videoplayer.data.api.RetrofitClient
 import com.silverorange.videoplayer.data.api.VideosApi
 import com.silverorange.videoplayer.data.repositories.VideosRepository
+import com.silverorange.videoplayer.databinding.ActivityMainBinding
 import com.silverorange.videoplayer.ui.videos.model.VideosViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     private val viewModel: VideosViewModel by lazy {
         val videosApi = RetrofitClient.getInstance().create(VideosApi::class.java)
@@ -16,9 +21,13 @@ class MainActivity : AppCompatActivity() {
         VideosViewModel(videosRepository)
     }
 
+    private var player: ExoPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         viewModel.getVideos()
         viewModel.currentVideo.observe(this) {
@@ -26,5 +35,33 @@ class MainActivity : AppCompatActivity() {
                 Log.d(MainActivity::class.java.simpleName, "Current video: ${video.title}")
             }
         }
+
+        loadVideosIntoPlayer()
+    }
+
+    private fun loadVideosIntoPlayer() {
+        viewModel.getVideos()
+        viewModel.videos.observe(this) {
+            it?.let { videos ->
+                val mediaItems = videos.map { video ->
+                    MediaItem.Builder()
+                        .setUri(video.hlsURL)
+                        .setMediaId(video.id)
+                        .build()
+                }
+                initializeVideoPlayer(mediaItems)
+            }
+        }
+    }
+
+    private fun initializeVideoPlayer(mediaItems: List<MediaItem>) {
+        player = ExoPlayer.Builder(this)
+            .build()
+            .also { player ->
+                binding.videoPlayer.player = player
+            }
+
+        mediaItems.forEach { player?.addMediaItem(it) }
+        player?.prepare()
     }
 }
