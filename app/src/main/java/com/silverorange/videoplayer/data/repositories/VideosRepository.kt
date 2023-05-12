5c876cea7ab503c647364810617dc0ec440a208e
José Eduardo Eguiguren <pepe.96.jose@gmail.com>
2023-05-12 14:31:40 -0700

    Sort videos by date

--- app/src/main/java/com/silverorange/videoplayer/data/repositories/VideosRepository.kt
+++ app/src/main/java/com/silverorange/videoplayer/data/repositories/VideosRepository.kt
@@ -3,6 +3,10 @@ package com.silverorange.videoplayer.data.repositories
 import com.silverorange.videoplayer.data.api.VideosApi
 import com.silverorange.videoplayer.data.model.Result
 import com.silverorange.videoplayer.data.model.Video
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+private const val DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSX"
 
 class VideosRepository(
     private val videosApi: VideosApi
@@ -13,7 +17,7 @@ class VideosRepository(
             val response = videosApi.getVideos()
             val videos = response.body()
             if (response.isSuccessful && videos != null) {
-                Result.Success(videos)
+                Result.Success(sortVideosByDate(videos))
             } else {
                 Result.Error(response.code(), response.message())
             }
@@ -21,4 +25,9 @@ class VideosRepository(
             Result.Failure(e)
         }
     }
+    private fun sortVideosByDate(videos: List<Video>): List<Video> =
+        videos.sortedBy {
+            val formatter = DateTimeFormatter.ofPattern(DATE_FORMAT)
+            LocalDateTime.parse(it.publishedAt, formatter)
+        }
 }
