ff9b19daf4aed6c11ac1fbef72c147e62ad3176e
José Eduardo Eguiguren <pepe.96.jose@gmail.com>
2023-05-12 13:18:00 -0700

    Create custom video player buttons layout with respective functionality

--- app/src/main/java/com/silverorange/videoplayer/ui/videos/MainActivity.kt
+++ app/src/main/java/com/silverorange/videoplayer/ui/videos/MainActivity.kt
@@ -3,8 +3,11 @@ package com.silverorange.videoplayer.ui.videos
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.ImageButton
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
+import com.silverorange.videoplayer.R
 import com.silverorange.videoplayer.data.api.RetrofitClient
 import com.silverorange.videoplayer.data.api.VideosApi
 import com.silverorange.videoplayer.data.repositories.VideosRepository
@@ -23,13 +26,18 @@ class MainActivity : AppCompatActivity() {
 
     private var player: ExoPlayer? = null
 
+    private lateinit var playButton: ImageButton
+    private lateinit var pauseButton: ImageButton
+    private lateinit var nextButton: ImageButton
+    private lateinit var previousButton: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        bindPlayerButtons()
 
-        viewModel.getVideos()
         viewModel.currentVideo.observe(this) {
             it?.let { video ->
                 Log.d(MainActivity::class.java.simpleName, "Current video: ${video.title}")
@@ -37,6 +45,13 @@ class MainActivity : AppCompatActivity() {
         }
 
         loadVideosIntoPlayer()
+    }
+
+    private fun bindPlayerButtons() = with(binding.videoPlayer) {
+        playButton = findViewById(R.id.play)
+        pauseButton = findViewById(R.id.pause)
+        nextButton = findViewById(R.id.next)
+        previousButton = findViewById(R.id.prev)
     }
 
     private fun loadVideosIntoPlayer() {
@@ -50,6 +65,8 @@ class MainActivity : AppCompatActivity() {
                         .build()
                 }
                 initializeVideoPlayer(mediaItems)
+                initializePlayerControls()
+                pauseVideo()
             }
         }
     }
@@ -63,5 +80,32 @@ class MainActivity : AppCompatActivity() {
 
         mediaItems.forEach { player?.addMediaItem(it) }
         player?.prepare()
+    }
+
+    private fun initializePlayerControls() {
+        playButton.setOnClickListener { playVideo() }
+        pauseButton.setOnClickListener { pauseVideo() }
+        nextButton.setOnClickListener { nextVideo() }
+        previousButton.setOnClickListener { previousVideo() }
+    }
+
+    private fun playVideo() {
+        player?.play()
+        playButton.visibility = View.GONE
+        pauseButton.visibility = View.VISIBLE
+    }
+
+    private fun pauseVideo() {
+        player?.pause()
+        playButton.visibility = View.VISIBLE
+        pauseButton.visibility = View.GONE
+    }
+
+    private fun nextVideo() {
+        player?.seekToNext()
+    }
+
+    private fun previousVideo() {
+        player?.seekToPreviousMediaItem()
     }
 }
